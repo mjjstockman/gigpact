107bdac25a89fa4d1646abc2fd97719980705508
mjjstockman <mjjstockman@gmail.com>
2025-06-14 12:03:40 +0100

    add failing test for displaying error when email format is invalid

--- __tests__/sign-up-form.test.tsx
+++ __tests__/sign-up-form.test.tsx
@@ -1,11 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { SignUpForm } from '../components/sign-up-form';
 
 describe('Rendering', () => {
   it('renders username, email, password, confirm password inputs, and register button', () => {
     render(<SignUpForm />);
-
     expect(screen.getByLabelText(/username/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/^password$/i)).toBeInTheDocument();
@@ -13,5 +12,39 @@ describe('Rendering', () => {
     expect(
       screen.getByRole('button', { name: /Sign Up/i })
     ).toBeInTheDocument();
+  });
+
+  describe('input errors', () => {
+    it('shows error when email format is invalid', async () => {
+      const mockOnSubmit = jest.fn(); // optionally type this later
+
+      render(<SignUpForm onSubmit={mockOnSubmit} />);
+
+      fireEvent.input(screen.getByLabelText(/username/i), {
+        target: { value: 'testuser' }
+      });
+
+      fireEvent.input(screen.getByLabelText(/email/i), {
+        target: { value: 'invalid-email' }
+      });
+
+      fireEvent.input(screen.getByLabelText(/^password$/i), {
+        target: { value: 'Password1!' }
+      });
+
+      fireEvent.input(screen.getByLabelText(/repeat password/i), {
+        target: { value: 'Password1!' }
+      });
+
+      fireEvent.click(screen.getByRole('button', { name: /sign up/i }));
+
+      await waitFor(() => {
+        expect(mockOnSubmit).not.toHaveBeenCalled();
+      });
+
+      expect(
+        await screen.findByText(/invalid email format/i)
+      ).toBeInTheDocument();
+    });
   });
 });
