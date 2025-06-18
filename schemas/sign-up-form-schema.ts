73a98147d42c3547b8aa24b1d65b6a52f6fb0377
mjjstockman <mjjstockman@gmail.com>
2025-06-18 15:18:34 +0100

    add and pass test for email empty error message

--- schemas/sign-up-form-schema.ts
+++ schemas/sign-up-form-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const signUpSchema = z.object({
-  email: z.string().min(1, 'Email is required').email('Invalid email format')
+  email: z.string().nonempty('Email is required').email('Invalid email format')
 });
 
 export type SignUpFormData = z.infer<typeof signUpSchema>;
